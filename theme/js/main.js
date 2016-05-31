71544fad7a40fd9daf6d922013299318d7350448
Vincent <vinz2007@hotmail.com>
2016-05-31 16:54:13 +0200

    edit following ops conversation

--- theme/js/main.js
+++ theme/js/main.js
@@ -46,8 +46,8 @@ $(function () {
     });
     // It is very important that this does NOT point to a 301 redirect url like `oecdbetterlifeindex.org`
     // without the `www` because (Mobile) Safari and IE lose everything after the hash.
-    var url = $(this).attr('href') + sliderValues;
-    window.location = url;
+    var url = $(this).attr('href') + sliderValues ;
+    window.open(url,'_blank');
     e.preventDefault();
     e.stopPropagation();
     return false;
